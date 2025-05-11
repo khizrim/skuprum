6137fcdf6883a63b8d5722740fd577db243d4ce7
Khizri Makhmudov <khizrim@khizrim.ru>
2025-05-11 20:38:48 +0300

    Add Yandex Metrika integration to the root component
    Inserted Yandex Metrika tracking code and a noscript fallback to enable analytics functionality. Ensured it does not interfere with existing components or the development environment setup.

--- src/root.tsx
+++ src/root.tsx
@@ -6,22 +6,49 @@ import { isDev } from '@builder.io/qwik';
 import './global.css';
 
 export default component$(() => {
-  /**
-   * The root of a QwikCity site always start with the <QwikCityProvider> component,
-   * immediately followed by the document's <head> and <body>.
-   *
-   * Don't remove the `<head>` and `<body>` elements.
-   */
-
   return (
     <QwikCityProvider>
       <head>
-        <meta charset="utf-8" />
+        <meta charSet="utf-8" />
         {!isDev && <link rel="manifest" href={`${import.meta.env.BASE_URL}manifest.json`} />}
         <RouterHead />
+
+        {/* ВСТАВКА ЯНДЕКС МЕТРИКИ */}
+        <script
+          dangerouslySetInnerHTML={`
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){
+              (m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(101748501, "init", {
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          `}
+        />
       </head>
       <body lang="ru" class="flex flex-col min-h-screen">
         <RouterOutlet />
+
+        {/* <noscript> часть метрики */}
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/101748501"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+
         {!isDev && <ServiceWorkerRegister />}
       </body>
     </QwikCityProvider>
