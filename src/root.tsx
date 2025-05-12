91ae136caa0446986e0e79e202ebe6fd6c8a65c0
Khizri Makhmudov <khizrim@khizrim.ru>
2025-05-12 09:11:57 +0300

    Refactor root component by removing Yandex Metrika integration and updating meta tag attributes

--- src/root.tsx
+++ src/root.tsx
@@ -9,46 +9,12 @@ export default component$(() => {
   return (
     <QwikCityProvider>
       <head>
-        <meta charSet="utf-8" />
+        <meta charset="utf-8" />
         {!isDev && <link rel="manifest" href={`${import.meta.env.BASE_URL}manifest.json`} />}
         <RouterHead />
-
-        {/* ВСТАВКА ЯНДЕКС МЕТРИКИ */}
-        <script
-          dangerouslySetInnerHTML={`
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){
-              (m[i].a=m[i].a||[]).push(arguments)};
-              m[i].l=1*new Date();
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-              }
-              k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-              k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(101748501, "init", {
-              clickmap:true,
-              trackLinks:true,
-              accurateTrackBounce:true
-            });
-          `}
-        />
       </head>
       <body lang="ru" class="flex flex-col min-h-screen">
         <RouterOutlet />
-
-        {/* <noscript> часть метрики */}
-        <noscript>
-          <div>
-            <img
-              src="https://mc.yandex.ru/watch/101748501"
-              style={{ position: 'absolute', left: '-9999px' }}
-              alt=""
-            />
-          </div>
-        </noscript>
-
         {!isDev && <ServiceWorkerRegister />}
       </body>
     </QwikCityProvider>
