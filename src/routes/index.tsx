82cbe33b5167791523fd5b74d1f885f699164962
Khizri Makhmudov <khizrim@khizrim.ru>
2025-05-15 20:26:58 +0300

    Add Yandex.Metrika Integration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -98,8 +98,35 @@ export const head: DocumentHead = {
       content: '#fff',
     },
   ],
-  links: [],
+  links: [
+    {
+      rel: 'preconnect',
+      href: 'https://mc.yandex.ru',
+    },
+  ],
   scripts: [
+    {
+      key: 'text/javascript',
+      props: {
+        type: 'text/javascript',
+      },
+      script: `
+        (function (m, e, t, r, i, k, a) {
+          m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+          m[i].l = 1 * new Date();
+          for (var j = 0; j < document.scripts.length; j++) {
+            if (document.scripts[j].src === r) return;
+          }
+          k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+        ym(101890997, "init", {
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true
+        });
+      `,
+    },
     {
       key: 'application/ld+json',
       props: {
